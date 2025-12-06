9f840dfe9a0bd5d078571a5e27b7c3025527c7bc
caliniya <caliniya@outlook.com>
2025-12-06 17:23:55 +0800

    update

---
+++ android-old/src/armadax/AndroidLauncher.java
@@ -0,0 +1,38 @@
+package caliniya.armadax;
+
+import android.os.Bundle;
+
+import arc.ApplicationListener;
+import arc.backend.android.AndroidApplicationConfiguration;
+import cat.ereza.customactivityoncrash.CustomActivityOnCrash;
+import cat.ereza.customactivityoncrash.config.CaocConfig;
+import arc.backend.android.AndroidApplication;
+import caliniya.armadax.Armadax;
+import caliniya.armadax.ErrorActivity;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        CaocConfig.Builder.create()
+            .enabled(true)
+            .errorActivity(ErrorActivity.class)
+            .apply();
+            
+        initialize(new Armadax(),
+        new AndroidApplicationConfiguration(){{
+            useImmersiveMode = true;
+            hideStatusBar = true;
+            useGL30 = true;
+            }}
+        );
+    }
+    
+    @Override
+    public void addListener(ApplicationListener appl) {
+        synchronized(this.getListeners()){
+            this.getListeners().add(appl);
+        }
+    }
+}
